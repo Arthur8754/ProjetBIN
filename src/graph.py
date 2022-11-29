592434fefb3a3fadf775efe00667f8303d938e0a
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2022-11-29 18:11:19 -0500

    Structure de graphe (fin)

--- src/graph.py
+++ src/graph.py
@@ -1,3 +1,8 @@
+"""
+Dans cette classe, on implémente la structure de graphe qui sera utile dans les algorithmes de l'article à résumer.
+Pour construire cette classe, je me suis inspiré de "util_gro_tp2.py" qu'on avait fait dans le TP2 de GRO l'an dernier à TN.
+"""
+
 class Node:
     """
     Classe associée à un noeud du graphe
@@ -17,6 +22,17 @@ class Node:
             noeud.voisins[self.numero] = self #on ajoute le noeud courant dans les voisins de noeud
         else:
             print("WARNING : les deux noeuds sont déjà reliés.")
+        return noeud #on retourne pour mettre à jour les 2 noeuds en même temps
+
+    def dislink_noeuds(self, noeud):
+        """
+        Casse le lien qu'il y avait entre le noeud courant et le noeud "noeud"
+        """
+        if noeud.numero in self.voisins.keys():
+            del self.voisins[noeud.numero] #on supprime noeud des voisins du noeud courant
+            del noeud.voisins[self.numero] #on supprime le noeud courant des voisins de noeud
+        else:
+            print("WARNING : les deux ne sont pas reliés, rien à enlever.")
         return noeud
     
 
@@ -52,6 +68,15 @@ class GrapheNonOriente:
         else:
             print("WARNING : l'un des noeuds n'est pas dans le graphe.")
 
+    def enlever_arete(self, noeud1, noeud2):
+        """
+        Enlève l'arête qui liait noeud1 et noeud2
+        Si l'un des 2 noeuds n'est pas dans le graphe, rien ne se passe.
+        """
+        if noeud1.numero in self.noeuds.keys() and noeud2.numero in self.noeuds.keys():
+            self.noeuds[noeud1.numero] = self.noeuds[noeud2.numero].dislink_noeuds(noeud1) #on supprime le lien entre noeud1 et noeud2.
+        else:
+            print("WARNING : l'un des noeuds n'est pas dans le graphe.")
 
 def main():
     """
@@ -73,6 +98,7 @@ def main():
     graphe.add_noeud(noeud2)
     graphe.add_noeud(noeud3)
 
+    print("Noeuds du graphe construit :")
     print(graphe.noeuds)
     print("")
 
@@ -80,11 +106,22 @@ def main():
     graphe.add_arete(noeud1, noeud2)
     graphe.add_arete(noeud1, noeud3)
     graphe.add_arete(noeud2, noeud3)
-    
+
+    print("Voisins pour chaque noeud :")
     print("Voisins de 0 : ",graphe.noeuds[0].voisins)
     print("Voisins de 1 : ",graphe.noeuds[1].voisins)
     print("Voisins de 2 : ",graphe.noeuds[2].voisins)
     print("Voisins de 3 : ",graphe.noeuds[3].voisins)
+    print("")
+
+    """
+    On va enlever une des arêtes désormais. On va enlever l'arête entre 1 et 3.
+    """
+
+    print("Suppression de l'arête entre 1 et 3 :")
+    graphe.enlever_arete(noeud1, noeud3)
+    print("Nouveaux voisins de 1 : ",graphe.noeuds[1].voisins)
+    print("Nouveaux voisins de 3 : ",graphe.noeuds[3].voisins)
 
 if __name__ == "__main__":
     main()
