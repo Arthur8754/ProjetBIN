23c8a506dd4133f8fd78070b37b030d0f6742c7e
Victor Michon <victor.michon@telecomnancy.eu>
2022-12-04 14:35:59 -0500

    Matrice Confusion

--- src/kmeans.py
+++ src/kmeans.py
@@ -1,12 +1,14 @@
 """
 Dans cette classe, on va appliquer l'algorithme des KMeans sur nos gènes. Plus précisément, on va l'appliquer sur Levenshtein.
 """
+import pandas as pd
 
 import distances
 from sklearn.cluster import KMeans
 import readSimulateGene
 import numpy as np
 from sklearn.metrics import confusion_matrix
+
 
 class kmeans:
 
@@ -75,6 +77,39 @@ class kmeans:
         accuracy = round(100*np.sum(np.diag(conf_matrix))/np.sum(conf_matrix),1) #somme des diagos / somme de tous les éléments
         print(f"Accuracy : {accuracy} %")
 
+
+def confusion(Fpredites, Fattendues):
+    TruePositive,TrueNegative,FalsePositive,FalseNegative = 0,0,0,0
+    for i in range(len(Fpredites)):
+        fp1, fa1 = Fpredites[i], Fattendues[i]
+        for j in range(i+1,len(Fpredites)):
+            fp2, fa2 = Fpredites[j], Fattendues[j]
+            # True positive
+            if fp1 == fp2 and fa1==fa2 :
+                TruePositive +=1
+            # False positive
+            elif fp1 == fp2 and fa1!=fa2 :
+                FalsePositive += 1
+            # True positive
+            elif fp1 != fp2 and fa1!=fa2 :
+                TrueNegative += 1
+            # False negative
+            else : FalseNegative += 1
+    return TruePositive,TrueNegative,FalsePositive,FalseNegative
+
+def perf_confusion(TP,TN,FP,FN):
+    N=TP+TN+FP+FN
+    accuracy = (TP+TN)/N
+    recall = TP/(TP+FN)
+    precision = TP/(TP+TN)
+    return accuracy,recall,precision
+
+def confusion_print(TP, TN, FP, FN):
+    array = [[TN, FN], [FP, TP]]
+    df = pd.DataFrame(array, index=["Negative Prediction", "Positive Prediction"],
+                      columns=["Negative Reality", "Positive Reality"])
+    print(df)
+
 def main():
     rsg = readSimulateGene.readSimulateGene()
     rsg.generate_sequences()
@@ -90,7 +125,13 @@ def main():
     print(km.prediction)
     print("Familles attendues :")
     print(km.familles)
-    km.eval_perf()
+
+    TP, TN, FP, FN = confusion(km.prediction, km.familles)
+    confusion_print(TP, TN, FP, FN)
+    accuracy, recall, precision = perf_confusion(TP,TN,FP,FN)
+    print("accuracy :", accuracy)
+    print("recall :", recall)
+    print("precision :", precision)
 
 if __name__=="__main__":
     main()
